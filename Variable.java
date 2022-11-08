03a231b30048dd4ea7174d06aec867ab71102ce1
Ibrar ul haque <117748816+ibrar153@users.noreply.github.com>
2022-11-08 11:21:16 -0800

    Q1 mutiple variable

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable
+{ public static void main (String args[])
+{
+	String a = "Ibrar ul haque";
+	int b =56655;
+	byte c =127;
+	short d=5555;
+	float e =3.55f;
+	double f= 3.5555;
+	boolean g =true;
+	char h= '+';
+	System.out.println("String = "+a);
+	System.out.println("int      = "+b);
+	System.out.println("byte    = "+c);
+	System.out.println("short   = "+d);
+	System.out.println("float    = "+e);
+	System.out.println("double = "+f);
+	System.out.println("boolean= "+g);
+	System.out.println("char     =  "+h);
+	
+}
+}
